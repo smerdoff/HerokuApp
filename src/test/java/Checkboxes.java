16e049d8fb49527e827d095e82e01deffa8066ae
smerdoff <eugenesmerdov17@gmail.com>
2020-04-17 02:46:34 +0300

    Edited Checkboxes tests

--- src/test/java/Checkboxes.java
+++ src/test/java/Checkboxes.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class Checkboxes {
     @Test
@@ -36,8 +38,8 @@ public class Checkboxes {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/checkboxes\n");
-        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("(//*[@type='checkbox'])[2]")));
-        WebElement option2 = driver.findElement(By.xpath("(//*[@type='checkbox'])[2]"));
+        List<WebElement> listofelements = driver.findElements(By.xpath("(//*[@type = 'checkbox'])"));
+        WebElement option2 = listofelements.get(1);
         Assert.assertEquals(true, option2.isSelected(), "Чекбокс№2 не выбран");
         driver.quit();
     }
