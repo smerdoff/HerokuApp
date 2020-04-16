4654820c0cb72352e9fca207b9bd536cb4306d85
smerdoff <eugenesmerdov17@gmail.com>
2020-04-16 23:22:28 +0300

    Add Checboxes tests

---
+++ src/test/java/Checkboxes.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Checkboxes {
+
+    @Test
+    public void Checktest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+    }
+
+
+}
